89aeeedc472a90f1cd41e8722a2c30eded8f29ea
Kirman442 <kirill@irminlabs.com>
2025-04-21 14:49:13 +0200

    topLevelAwait added

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-wasm";
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/mobile-network-map/",
   plugins: [
     react(),
-    wasm()
+    wasm(),
+    topLevelAwait()
   ],
   worker: {
-    plugins: [
+    plugins: () => [
       wasm(),
       topLevelAwait()
     ]
   },
   optimizeDeps: {
-    exclude: ['parquet-wasm']
+    exclude: ['parquet-wasm', 'apache-arrow']
   },
   build: {
     chunkSizeWarningLimit: 1000,
   },
-})
+});
