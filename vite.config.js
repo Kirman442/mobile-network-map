0b07ec6e333210607976b506c75ee9978df63796
Kirman442 <kirill@irminlabs.com>
2025-04-21 17:39:23 +0200

    inline vorker with CDN versions of the libraries

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from 'vite-plugin-top-level-await'
-import { resolve } from 'path'
 
 export default defineConfig({
   base: "/mobile-network-map/",
@@ -22,13 +21,8 @@ export default defineConfig({
     exclude: ['parquet-wasm', 'apache-arrow']
   },
   build: {
-    chunkSizeWarningLimit: 1000,
-    assetsInlineLimit: 0,
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    chunkSizeWarningLimit: 1500,
+    target: 'esnext',
+    sourcemap: true
   }
 })
