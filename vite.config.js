c2165fbb59e521dbc0038412ced9cdfc660d8bd0
Kirman442 <kirill@irminlabs.com>
2025-04-27 12:24:02 +0200

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   base: "/mobile-network-map",
   plugins: [
     react(),
-    wasm()
+    wasm(),
+    topLevelAwait()
   ],
-  optimizeDeps: {
-    exclude: ['parquet-wasm'] // Добавьте эту строку
+  worker: {
+    plugins: () => [
+      wasm(),
+      topLevelAwait()
+    ],
+    format: 'es',
+    // format: 'iife'
   },
-})
+  optimizeDeps: {
+    exclude: ['parquet-wasm'],
+    include: ['apache-arrow']
+  },
+  build: {
+    // minify: false,
+    target: 'esnext',
+    // target: 'es2020',
+    chunkSizeWarningLimit: 1800,
+    copyPublicDir: true,
+    modulePreload: {
+      polyfill: true,
+    },
+    assetsInlineLimit: 0, // Не инлайнить WASM как base64
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'parquet-wasm': ['parquet-wasm']
+        }
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+});
