02389aa2f301729755dd77d6888d26754a76b7f0
Kirman442 <kirill@irminlabs.com>
2025-04-30 19:35:18 +0200

    /mobile-network-map for github

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import wasm from "vite-plugin-wasm"
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-  base: "/",
+  base: "/", // /mobile-network-map
   plugins: [
     react(),
     wasm(),
