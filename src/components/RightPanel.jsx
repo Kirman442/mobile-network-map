bb9d099fa1d48886f563e8aaa1249b5350304b78
Kirman442 <kirill@irminlabs.com>
2025-05-01 20:44:33 +0200

    formatNumber function has been edited

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -12,7 +12,10 @@ const LegendPanel = ({ mapStyle, setMapStyle, activeColorSchemeKey, setActiveCol
     const formatNumber = (num) => {
         if (!num) return '0';
 
-        if (num >= 1000) {
+        if (num >= 1000000) {
+            return `${(num / 1000000).toFixed(2)} Mio`;
+        }
+        else if (num >= 1000) {
             return `${(num / 1000).toFixed(1)}K`;
         }
         return num.toString();
